915a28a229e5272c51f462931b500e0b8e6ebc26
seonghoon jeong <jeongseonghoon4537@gmail.com>
2026-01-29 10:08:17 +0900

    feat: 6장 종료

--- proptypes-test/src/main.tsx
+++ proptypes-test/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import App from "./App01";
+// import App from "./App02";
+//import App from "./App03";
+//import App from "./App04";
+//import App from "./App05";
+//import App from "./App06";
+// import App from "./App07";
+// import App from "./App08";
+import App from "./App09";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
